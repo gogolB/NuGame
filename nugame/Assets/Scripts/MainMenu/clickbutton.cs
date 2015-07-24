8c9988e44ef00e951e1c42b030330a2e60fa0034
gogolB <sbhat006@ucr.edu>
2015-07-24 10:09:54 -0700

    First Commit
    First Commit, The Git Ignore as well as the settings for the player have
    been updated.

---
+++ nugame/Assets/Scripts/MainMenu/clickbutton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickbutton : MonoBehaviour {
+
+	bool dispmenu = false;
+	private Color ourColor;
+	public GameObject camert;
+	public GameObject targ = null;
+	float timemit = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		camert = GameObject.Find ("Main Camera");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timemit += Time.deltaTime;	
+	}
+
+	void OnMouseEnter()
+	{
+
+		ourColor = GetComponent<Renderer>().material.GetColor("_Color");
+
+		if(ourColor.a >= 0.659f)
+		{
+			//Debug.Log("Hello World");
+			ourColor.a = 1.0f;
+			GetComponent<Renderer>().material.SetColor ("_Color", ourColor);
+		}
+	}
+
+	void OnMouseOver()
+	{
+			GetComponent<AudioSource> ().Play ();
+	}
+
+	void OnMouseExit()
+	{
+		ourColor = GetComponent<Renderer>().material.GetColor("_Color");
+		
+		if(ourColor.a >= 0.67f)
+		{
+			//Debug.Log("Hello World");
+			ourColor.a = 0.66f;
+			GetComponent<Renderer>().material.SetColor ("_Color", ourColor);
+		}
+	}
+}
