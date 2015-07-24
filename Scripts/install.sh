1a9a71dbaa46fe494e22520450c6f9af5ebfaecf
gogolB <sbhat006@ucr.edu>
2015-07-24 10:56:19 -0700

    Updated the installer

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,7 +3,7 @@
 # Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 echo 'Downloading from http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg'
-curl -O http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg
+curl -O http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity-5.1.2f1.pkg -target /
