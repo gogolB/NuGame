04404a31da1095547649c29dcdae24d1b49a5948
gogolB <sbhat006@ucr.edu>
2015-07-24 12:10:21 -0700

    Added comment to install.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,6 +2,7 @@
 
 # Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
+# This path will need to be set everytime unity updates their builder. We could also just get a copy and host it on one of our own servers, Probably will have a slower download time.
 echo 'Downloading from http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg'
 curl -O http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
 
