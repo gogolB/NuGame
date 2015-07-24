0288356562d86056d9e8691eba286c227c23a32f
gogolB <sbhat006@ucr.edu>
2015-07-24 14:35:28 -0700

    Just started work on the character
    Created two basic char scripts,
    Added in a texture for the floor,
    Created a basic level to work in for character devel.

---
+++ nugame/Assets/Scripts/Player/Player_Camera_Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Camera_Controller : MonoBehaviour {
+
+	public float angleOfIncidence = 60;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ nugame/Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour
+{
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	void Update() {
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		// TODO Need to change this from unity's input system to our own input system.
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+
+		// If we are not on the ground, get us to the ground.
+		if (!controller.isGrounded)
+			moveDirection.y -= 20;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
