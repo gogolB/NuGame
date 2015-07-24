e49bf022e60053fd0a2316c4cd2357547e6291d2
gogolB <sbhat006@ucr.edu>
2015-07-24 15:13:51 -0700

    added the player camera controller

--- nugame/Assets/Scripts/Player/Player_Camera_Controller.cs
+++ nugame/Assets/Scripts/Player/Player_Camera_Controller.cs
@@ -1,16 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// TODO only do calculations when the variables are dirty, otherwise don't. Saves on compute time.
 public class Player_Camera_Controller : MonoBehaviour {
 
-	public float angleOfIncidence = 60;
+
+	[Range(0.0f, 90.0f)]
+	[Tooltip("This is the angle formed by the forward vector of the camera and the ground plane.")]
+	public float angleOfIncidence = 60f;
+
+	[Range(0.0f, 50.0f)]
+	[Tooltip("This is the distance from the camera to the player.")]
+	public float armLength = 10f;
+
+	[Tooltip("This is allows the player to move the camera with the right mouse button.")]
+	public bool allowPlayerCameraRotation = false;
+
+	[Range(1.0f, 50.0f)]
+	[Tooltip("This is how sensitive the mouse is to player movement.")]
+	public float mouseSensitivity = 5.0f;
+
+	[Range(0.0f, 360.0f)]
+	public float groundRotation = 180.0f;
+
+	[Tooltip("What is this camera looking at.")]
+	public GameObject target;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		if (target == null) {
+			Debug.LogError("NO PLAYER TARGET SET FOR CAMERA. PLEASE SET TARGET.");
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (allowPlayerCameraRotation && Input.GetMouseButton(1)) {
+			groundRotation += Input.GetAxis("Mouse X") * mouseSensitivity;
+			groundRotation = Mathf.Clamp(groundRotation, 0.0f, 360.0f);
+		}
+
+		float height = Mathf.Sin(Mathf.Deg2Rad * angleOfIncidence) * armLength;
+		float groundDist = Mathf.Cos(Mathf.Deg2Rad * angleOfIncidence) * armLength;
+
+
+		float groundX = Mathf.Sin (Mathf.Deg2Rad * groundRotation) * groundDist;
+		float groundZ = Mathf.Cos (Mathf.Deg2Rad * groundRotation) * groundDist;
+
+		this.transform.localPosition = new Vector3 (groundX, height, groundZ);
+		this.transform.LookAt (target.transform.position);
 	}
 }
