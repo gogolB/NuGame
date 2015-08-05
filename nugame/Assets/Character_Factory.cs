393ba7b2c92123db436f0f7418356de6463b2e6b
gogolB <sbhat006@ucr.edu>
2015-08-04 17:32:16 -0700

    Started working on the Character Factory
    This component will create a player character including all the data
    that is needed to drive the player's character tree so that the game can
    happen.

---
+++ nugame/Assets/Character_Factory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class Character_Factory : MonoBehaviour 
+{
+	public enum Character{
+		Ingrid_Arteaga
+	}
+
+	public Character charToBuild = Character.Ingrid_Arteaga;
+	// Use this for initialization
+	void Start () 
+	{
+		XmlDocument doc =  new XmlDocument();
+		doc.Load(Application.dataPath + "/Resources/Characters/Ingrid_Arteaga.char");
+		XmlNode root = doc.DocumentElement;
+		printChildren(root);
+
+	}
+
+	private void printChildren(XmlNode baseNode)
+	{
+		int numChildren = baseNode.ChildNodes.Count;
+		Debug.Log("Node " + baseNode.Name +" has of children: " + numChildren);
+		Debug.Log(baseNode.InnerText);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
