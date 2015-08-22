ec6bc40819f913a23c1400d44ca0c52e16d91f17
gogolB <sbhat006@ucr.edu>
2015-08-22 01:04:07 -0700

    Another fix

--- Scripts/buildChecker.sh
+++ Scripts/buildChecker.sh
@@ -2,13 +2,8 @@
 
 # This file is responsible for checking the output log from the build.
 
-PATTERN="Compilation failed"
-
-ls -al
-ls -al $(pwd)
-
-if grep -q $PATTERN "$(pwd)/unity.log"; then
-    exit 0
-else
+if grep -q "Compilation failed" unity.log; then
     exit 1
+else
+    exit 0
 fi
