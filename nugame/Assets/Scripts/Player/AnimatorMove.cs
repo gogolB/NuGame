203ef47ba65bd0ea61f59b85d812625d36a8b4cc
gogolB <sbhat006@ucr.edu>
2015-09-03 23:39:49 -0700

    Got rid of all the old animations added in a new character
    Removed all the old animations and added in a new character. Working on
    the female counterpart.

---
+++ nugame/Assets/Scripts/Player/AnimatorMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorMove : MonoBehaviour {
+
+	void OnAnimatorMove() {
+		Animator animator = GetComponent<Animator>();
+		if (animator) {
+			Vector3 newPosition = transform.parent.position;
+			newPosition.z = this.transform.position.z;
+			newPosition.x = this.transform.position.x;
+			transform.parent.position = newPosition;
+		}
+	}
+}
